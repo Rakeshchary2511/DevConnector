3c078bd60d66579ab444127b19ae02b28cbc3aaf
Rakeshchary2511 <rakeshchary251199@gmail.com>
2020-09-28 15:31:31 +0530

    All routes and User Schema built along with registering user

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ const app = express();
 
 // Connect Database
 connectDB();
+
+//Init moddleware
+app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("API running"));
+
+//Define Routes
+app.use("/api/users", require("./routes/api/users.js"));
+app.use("/api/auth", require("./routes/api/auth.js"));
+app.use("/api/posts", require("./routes/api/posts.js"));
+app.use("/api/profiles", require("./routes/api/profiles.js"));
 
 const PORT = process.env.PORT || 5000;
 
